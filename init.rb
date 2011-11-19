728bfb5aa3d870f7db18c27af83a494e4fb28e28
Hernan Schmidt <hernan@plegnic.com>
2011-11-19 15:48:56 -0300

    show profile

--- init.rb
+++ init.rb
@@ -5,6 +5,23 @@ module HireMe
       env = ENV['SINATRA_ENV'] || 'development'
     end
 
+    helpers do
+      def build_highcharts_array(data)
+        array = "["
+        data.each_with_index do |datum, i|
+          inner_array = "["
+          inner_array << "'" + datum[:lang] + "'"
+          inner_array << ","
+          inner_array << datum[:percentage].to_s
+          inner_array << "]"
+          array << inner_array
+          array << "," unless i == data.size-1
+        end
+        array << "]"
+        array
+      end # end build_highcharts_array
+    end
+
     # mime_type :json, 'application/json'
 
     # before do
@@ -20,7 +37,18 @@ module HireMe
     # Return JSON or HTML representing
     # the Github user
     get '/:username.?:format?' do
+      @username = params[:username]
+      @title    = "#{@username}'s Profile"
 
+      stats = [
+        {lang: "Ruby", count: 10, percentage: (10.0/21.0)},
+        {lang: "PHP", count: 4, percentage: (4.0/21.0)},
+        {lang: "Python", count: 7, percentage: (7.0/21.0)}
+      ]
+
+      @lang_stats = build_highcharts_array(stats)
+
+      erb :profile
     end
 
   end
