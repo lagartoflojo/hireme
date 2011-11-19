50b688f7207d376ab0774e49cc79d0ad453808cf
Hernan Schmidt <hernan@plegnic.com>
2011-11-19 14:19:48 -0300

    Define routes

--- init.rb
+++ init.rb
@@ -5,15 +5,23 @@ module HireMe
       env = ENV['SINATRA_ENV'] || 'development'
     end
 
-    mime_type :json, 'application/json'
+    # mime_type :json, 'application/json'
 
-    before do
-      content_type :json
-    end
+    # before do
+    #   content_type :json
+    # end
 
+    # Show a search box
     get '/' do
       #hello
-  end
+    end
+
+
+    # Return JSON or HTML representing
+    # the Github user
+    get '/:username.?:format?' do
+
+    end
 
   end
 end
